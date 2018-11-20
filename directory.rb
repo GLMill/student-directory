67ed90a55eac2007afefbeb0b1d82c9aa13f2b6c
Georgia Millard <georgiamillardccm@gmail.com>
2018-11-20 12:59:57 +0000

    up to excercise 8 but have headache and so distracted :(

--- directory.rb
+++ directory.rb
@@ -1,26 +1,72 @@
+def input_students
 
-students = [
-['Mojo Jojo', :november],
-['White Diamond', :november],
-['Voldemort', :november],
-['The plague', :november],
-['Smith', :november],
-['James Bond', :november]
-]
-def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+    # create an empty array
+    students = []
+
+    while true do
+        puts "Please enter the names of the students"
+        puts "To finish, just hit return twice"
+        name = gets.chomp
+        if name.empty?
+            return students
+            break
+        end
+        puts "Please enter cohort"
+        cohort = gets.chomp
+        puts "star sign"
+        star_sign = gets.chomp
+        puts "food preference"
+        food = gets.chomp
+        puts "fears"
+        fears = gets.chomp
+      # add the student hash to the array
+      students << {name: name, cohort:cohort.to_sym, star_sign: star_sign, food: food, fears: fears}
+      puts "Now we have #{students.count} students"
+
+    end
+    # return the array of students
+    students
 end
 
-def print(multi_arr)
-    multi_arr.each do |arr|
-     puts "#{arr[0]} (#{arr[1]} cohort)"
+def specific_letter
+    puts "search by specific letter? yes no"
+    letter = gets.chomp.downcase
+    if(letter === 'yes')
+        puts "search by which letter?"
+        letter = gets.chomp.downcase
+        return letter
+    end
+end
+
+def print_header
+    puts "The students of Villains Academy".center(60, '-')
+end
+
+
+def print(hash_array, letter)
+    counter = 0
+    while counter < hash_array.length do
+    #multi_arr.each_with_index do |hash, index|
+    hash = hash_array[counter]
+    # puts hash[:name]
+        if letter
+            if hash[:name][0] === letter && hash[:name].length < 12
+                puts "#{counter + 1}. #{hash[:name]} (#{hash[:cohort]} cohort) star sign is #{hash[:star_sign]} they like #{hash[:food]} and are scared of #{hash[:fears]}"
+            end
+        elsif hash[:name].length < 12
+            puts "#{counter + 1}. #{hash[:name]} (#{hash[:cohort]} cohort) star sign is #{hash[:star_sign]} they like #{hash[:food]} and are scared of #{hash[:fears]}"
+        else
+        end   
+        counter += 1 
     end
 end 
+
 def print_footer(array)
-    puts "Overall, we have #{array.count} students enrolled. "
+    puts "Overall, we have #{array.count} students enrolled. ".center(80, '-')
 end
 
+students = input_students
+letter = specific_letter
 print_header
-print(students)
+print(students, letter)
 print_footer(students)
